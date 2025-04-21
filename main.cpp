598758b98593ea5cff5c7680989128b9b4d861c4
celv <cdelv@unal.edu.co>
2025-04-21 17:46:27 -0400

    add naive simulation

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 
 long int simulate_qtree(const int N, const int MAX_DEPTH, const int steps, const float dt, const float k, const bool save, const bool print, const int SAVE_FREQUENCY, const std::string BASE_DIR, const float MAX_RADIUS, const float SPACING, const float polydispersity);
 long int simulate_grid(const int N, const int MAX_DEPTH, const int steps, const float dt, const float k, const bool save, const bool print, const int SAVE_FREQUENCY, const std::string BASE_DIR, const float MAX_RADIUS, const float SPACING, const float polydispersity);
+long int simulate_naive(const int N, const int MAX_DEPTH, const int steps, const float dt, const float k, const bool save, const bool print, const int SAVE_FREQUENCY, const std::string BASE_DIR, const float MAX_RADIUS, const float SPACING, const float polydispersity);
 
 
 int main(int argc, char const *argv[])
@@ -27,20 +28,26 @@ int main(int argc, char const *argv[])
     const std::string BASE_DIR = "frames/";
     const float MAX_RADIUS  = 1.0f;
     const float SPACING = 1.5f * MAX_RADIUS;
-    long int performance1, performance2;  
+    long int performance1, performance2, performance3;  
     float polydispersity = 1.0;  // from 1.0 to infinity (how many times the biggest sphere can be compare with 1.0)
 
     //simulate_qtree(10000, MAX_DEPTH, steps, dt, k, save, true, SAVE_FREQUENCY, BASE_DIR, MAX_RADIUS, SPACING, 2.5);
     //simulate_grid(10000, MAX_DEPTH, steps, dt, k, save, true, SAVE_FREQUENCY, BASE_DIR, MAX_RADIUS, SPACING, 2.5);
     //return 0;
     
-    std::cout << "poly,N,qtree,grid" << std::endl;
+    std::cout << "poly,N,qtree,grid,naive" << std::endl;
     for (int j = 0; j < 4; j++) {
         for (int i = 0; i < 19; ++i) {
             int N = std::pow(2, i);
             performance1 = simulate_qtree(N, MAX_DEPTH, steps, dt, k, save, print, SAVE_FREQUENCY, BASE_DIR, MAX_RADIUS, SPACING, polydispersity);
             performance2 = simulate_grid(N, MAX_DEPTH, steps, dt, k, save, print, SAVE_FREQUENCY, BASE_DIR, MAX_RADIUS, SPACING, polydispersity);
-            std::cout << polydispersity << "," << N << "," << performance1 << "," << performance2 << std::endl; 
+            
+            if(N < 16384) {
+                performance3 = simulate_naive(N, MAX_DEPTH, steps, dt, k, save, print, SAVE_FREQUENCY, BASE_DIR, MAX_RADIUS, SPACING, polydispersity);
+                std::cout << polydispersity << "," << N << "," << performance1 << "," << performance2 << "," << performance3 << std::endl; 
+            }
+            else
+                std::cout << polydispersity << "," << N << "," << performance1 << "," << performance2 << std::endl; 
         }
          polydispersity += 0.75;
     }
@@ -317,6 +324,115 @@ long int simulate_qtree(const int N, const int MAX_DEPTH, const int steps, const
         if (save && step % SAVE_FREQUENCY == 0) {
             save_frame_qtree(BASE_DIR, spheres, qtree, LENGTH, HEIGHT, frame);
         }
+        if (step%SAVE_FREQUENCY == 0 && print){
+            auto end2 = std::chrono::steady_clock::now();
+            float elapsed_time2 = std::chrono::duration<float>(end2 - start2).count();
+            std::cout << step << "," << (N * SAVE_FREQUENCY) / elapsed_time2 << std::endl;
+            start2 = std::chrono::steady_clock::now();
+        }
+    }
+    
+    auto end = std::chrono::steady_clock::now();
+    float elapsed_time = std::chrono::duration<float>(end - start).count();
+    return (N * steps) / elapsed_time;
+}
+
+
+long int simulate_naive(const int N, const int MAX_DEPTH, const int steps, const float dt, const float k, const bool save, const bool print, const int SAVE_FREQUENCY, const std::string BASE_DIR, const float MAX_RADIUS, const float SPACING, const float polydispersity) {   
+    std::vector<sphere> spheres(N);
+    std::vector<float> ax(N, 0.0f);
+    std::vector<float> ay(N, 0.0f);
+    
+    const float HEIGHT = std::sqrt(N) * SPACING * 4 * polydispersity;
+    const float LENGTH = std::sqrt(N) * SPACING * 4 * polydispersity;
+    const int COLS = static_cast<int>(std::ceil(std::sqrt(N)));
+
+    std::random_device rd;            // seed source
+    std::mt19937 gen(rd());           // Mersenne Twister engine
+    std::uniform_real_distribution<float> dis(0.0f, 2.0f);
+    std::uniform_real_distribution<float> rad(MAX_RADIUS, polydispersity*MAX_RADIUS);
+    
+    // Initialize spheres in a grid.
+    for (int i = 0; i < N; i++) {
+        int row = i / COLS;
+        int col = i % COLS;
+
+        spheres[i].x = col * 2*polydispersity*MAX_RADIUS + polydispersity*MAX_RADIUS;
+        spheres[i].y = row * 2*polydispersity*MAX_RADIUS + polydispersity*MAX_RADIUS;
+        spheres[i].vx = -dis(gen);
+        spheres[i].vy = -dis(gen);
+        spheres[i].r = rad(gen);
+    }
+    
+    int frame = 0;
+
+    if (print) {
+        std::cout << "step,performance" << std::endl;
+    }
+    
+    // Simulation loop.
+    auto start = std::chrono::steady_clock::now();
+    auto start2 = std::chrono::steady_clock::now();
+    for (int step = 0; step < steps; ++step) {
+        
+        // Compute collision forces.
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; ++j) {
+                const float dx = spheres[i].x - spheres[j].x;
+                const float dy = spheres[i].y - spheres[j].y;
+                const float distance = std::hypot(dx, dy);
+                
+                const float minDistance = spheres[i].r + spheres[j].r;
+                if (distance < minDistance && distance > 0.0f) {                    
+                    const float nx = dx / distance;
+                    const float ny = dy / distance;
+                    const float overlap = minDistance - distance;
+                    ax[i] -= k * overlap * nx;
+                    ay[i] -= k * overlap * ny;
+                }
+            }
+
+            // Force with walls.
+            // Left wall: if (x - r < 0), then penetration = -(x - r).
+            float s = spheres[i].x - spheres[i].r;
+            if (s < 0) {
+                ax[i] += k * (-s);
+            }
+
+            // Right wall: if (x + r > LENGTH), then penetration = (x + r) - LENGTH.
+            s = spheres[i].x + spheres[i].r - LENGTH;
+            if (s > 0) {
+                ax[i] -= k * s;
+            }
+
+            // Bottom wall: if (y - r < 0), then penetration = -(y - r).
+            s = spheres[i].y - spheres[i].r;
+            if (s < 0) {
+                ay[i] += k * (-s);
+            }
+
+            // Top wall: if (y + r > HEIGHT), then penetration = (y + r) - HEIGHT.
+            s = spheres[i].y + spheres[i].r - HEIGHT;
+            if (s > 0) {
+                ay[i] -= k * s;
+            }
+        }
+        
+        // Update positions and velocities.
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            spheres[i].vx += dt * ax[i];
+            spheres[i].vy += dt * ay[i];
+            spheres[i].x  += dt * spheres[i].vx;
+            spheres[i].y  += dt * spheres[i].vy;
+            
+            // Reset forces.
+            ax[i] = 0.0f; 
+            ay[i] = 0.0f;
+        }
+
+
         if (step%SAVE_FREQUENCY == 0 && print){
             auto end2 = std::chrono::steady_clock::now();
             float elapsed_time2 = std::chrono::duration<float>(end2 - start2).count();
